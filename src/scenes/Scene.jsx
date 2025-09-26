f1b49b3ff8b4183ca3d5b8c5eb3b589aa27108b4
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-09-26 19:24:23 +0530

    Remove ground shadow text and plane from Scene
    Deleted the shadow text and ground plane elements from the Scene component, simplifying the scene's visual elements.

--- src/scenes/Scene.jsx
+++ src/scenes/Scene.jsx
@@ -17,26 +17,7 @@ function Scene() {
         MultiDisciplinary Designer
       </Text>
       
-      {/* Shadow Text on Ground */}
-      <Text
-        position={[0, -2, 0]}
-        fontSize={0.6}
-        color="#000000"
-        anchorX="center"
-        anchorY="middle"
-        maxWidth={6}
-        textAlign="center"
-        rotation={[Math.PI / 2, 0, 0]}
-        opacity={0.08}
-      >
-        MultiDisciplinary Designer
-      </Text>
-      
-      {/* Ground Plane */}
-      <mesh position={[0, -2.1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[20, 20]} />
-        <meshBasicMaterial color="#ffffff" transparent opacity={0.02} />
-      </mesh>
+     
       
       <ambientLight intensity={1.0} color="#ffffff" />
       <directionalLight position={[5, 5, 5]} intensity={0.5} />
