bc5735e256f21a08aad36a20909fefb791fbc025
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2026-01-21 16:26:33 +0530

    done.

---
+++ riveheart/riveheart/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  riveheart
+//
+//  Created by Vikas Raj on 21/01/26.
+//
+
+import SwiftUI
+import RiveRuntime
+import AVFoundation
+
+struct ContentView: View {
+    @State private var riveVM: RiveViewModel?
+    @State private var popPlayer: AVAudioPlayer?
+    
+    var body: some View {
+        ZStack {
+            // Background image - full screen, behind everything
+            Image("bg")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea(.all)
+            
+            // Rive animation on top
+            Group {
+                if let riveVM {
+                    riveVM.view()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .ignoresSafeArea(.all)
+                        .onTapGesture {
+                            playPop()
+                            // If your Rive file has a trigger input named "popTrigger", this will fire it too:
+                            // riveVM.triggerInput("popTrigger")
+                        }
+                } else {
+                    VStack(spacing: 12) {
+                        Text("Missing riveheart-2.riv")
+                            .font(.headline)
+                        Text("Add `riveheart.riv` to the app target (Copy Bundle Resources), then rebuild.")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding()
+                }
+            }
+        }
+        .onAppear {
+            // Avoid RiveRuntime fatalError in SwiftUI previews / when the resource isn't bundled yet.
+            if Bundle.main.url(forResource: "riveheart-2", withExtension: "riv") != nil {
+                riveVM = RiveViewModel(fileName: "riveheart-2")
+            }
+            preparePopSound()
+        }
+    }
+
+    private func preparePopSound() {
+        guard popPlayer == nil else { return }
+        guard let url = Bundle.main.url(forResource: "pop", withExtension: "wav") else { return }
+        popPlayer = try? AVAudioPlayer(contentsOf: url)
+        popPlayer?.prepareToPlay()
+    }
+
+    private func playPop() {
+        if popPlayer == nil { preparePopSound() }
+        popPlayer?.currentTime = 0
+        popPlayer?.play()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ riveheart/riveheart/riveheartApp.swift
@@ -0,0 +1,17 @@
+//
+//  riveheartApp.swift
+//  riveheart
+//
+//  Created by Vikas Raj on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct riveheartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
